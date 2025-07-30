d1cb6f40964781589b2dfc96e013d0169cd3d8e7
mikiCode <mikill.priv@gmail.com>
2025-07-30 20:38:38 +0200

    change db file location

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,7 +1,8 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "openaq.db")
+DB_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "openaq.db")
+DB_PATH = os.path.abspath(DB_PATH)
 
 
 def get_connection():
